d11c07a6fbb93d64ae8c7ac2a93b5e0c274905eb
Wytchwood <crawle.gideon@gmail.com>
2020-04-04 12:38:01 +0100

    asdads

--- Plugins/Dialog/Dialog.cpp
+++ Plugins/Dialog/Dialog.cpp
@@ -109,7 +109,7 @@ CNWSObject* Dialog::Hooks::GetSpeaker(CNWSDialog*pThis,
         newSpeaker = nullptr;
         return retVal;
     }
-    g_plugin->m_GetSpeakerHook.CallOriginal(pNWSObjectOwner, sSpeaker);
+    g_plugin->m_GetSpeakerHook->CallOriginal(pNWSObjectOwner, sSpeaker);
 }
 
 void Dialog::Hooks::SendDialogEntry(bool before, CNWSDialog *pThis,
